a9ca969babe2dd6deaee2f39aa5d5f497c3ad0a5
Victor Miguel <vthero89@gmail.com>
2025-11-18 14:03:21 -0300

    Quiz quase Finalizado

---
+++ vue-project/src/components/quizstart.vue
@@ -0,0 +1,66 @@
+<template>
+    <header></header>
+    <main class="quizstart">
+        <h2>Quiz</h2>
+        <p>{{ apresentacao }}</p>
+        <button type="button" @click="trocarTela">Começar</button>
+    </main>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+
+const emit = defineEmits(['trocarTela'])
+
+const apresentacao = ref(
+  'Bem-vindo ao Quiz sobre a Bacia Hidrográfica do Rio Ceará-Mirim. Este questionário tem como objetivo avaliar seus conhecimentos sobre aspectos geográficos, ambientais e sociais relacionados à bacia. Leia atentamente e boa realização.'
+)
+
+
+function trocarTela() {
+  emit('trocarTela')
+}
+</script>
+<style>
+    body{
+ background-color: var(--bg-body);      
+    }
+</style>
+
+
+<style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+*{
+    font-family: 'Poppins',sans-serif;
+}
+main{
+    position: relative;
+    top: 10vh;
+    width: 50vw;
+    left: 5vw;
+}
+h2{
+    font-size: 2em;
+}
+button{
+        background-color: var(--bg-but) ;
+        color: var(--txt-but);
+        width: 240px;
+        height: 44px;
+        border-radius: 200px;
+        border: none;
+        font-size: 1em;
+        font-weight: bold;
+        transition: background-color 0.1s ease-in;
+        box-shadow: 0px 3px 3px var(--color-boxs);
+    &:hover{
+        background-color: #0A142F;
+    }
+    
+    }
+p{
+    width: 50vw;
+}
+</style>
---
+++ vue-project/src/views/quiz.vue
@@ -0,0 +1,16 @@
+<script setup>
+    
+    import { ref } from 'vue';
+    import quizstart from '@/components/quizstart.vue';
+    import quizperguntas from '@/components/quizperguntas.vue';
+
+
+    const teladisplay = ref('apresentar')
+</script>
+<template>
+
+    <quizstart v-if="teladisplay === 'apresentar'" @trocarTela="teladisplay = 'perguntas'"/>
+    <quizperguntas v-if="teladisplay === 'perguntas'"/>
+
+</template>
+<style scoped></style>
